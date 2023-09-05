9297f0bb8d13272e1613908941f0fdb5feea636a
Mustafa Alelg <me@mstfelg.com>
2023-09-05 20:28:25 +0300

    Doc string minor fixes

--- deriv/2-mc-contrapos.py
+++ deriv/2-mc-contrapos.py
@@ -2,7 +2,8 @@
 
 from sympy import symbols
 
-from barhana.rules import cderiv, dneg, ideriv, let_ante, let_not, modp, show_cons
+from barhana.rules import (cderiv, dneg, ideriv, let_ante, let_not, modp,
+                           show_cons)
 
 p,q = symbols('p q')
 pr = [
@@ -10,10 +11,11 @@ pr = [
 ]
 conc = ~q >> ~p
 
-if1 = let_ante(conc)
-cl1 = show_cons(conc)
-if2 = let_not(cl1)
-st1 = dneg(if2)
-st2 = modp(st1, pr[0])
-pf1 = ideriv(st2, if1)
-conc_pf = cderiv(if1,pf1)
+if if1 := let_ante(conc):
+    cl1 = show_cons(conc)
+
+    if if2 := let_not(cl1):
+        st1 = dneg(if2)
+        st2 = modp(st1, pr[0])
+    pf1 = ideriv(st2, if1)
+conc_pf = cderiv(if1, pf1)
--- deriv/3-mc-neg-cond.py
+++ deriv/3-mc-neg-cond.py
@@ -2,7 +2,7 @@
 
 from sympy import symbols
 
-from barhana.rules import demorgan, let_not, vacuous
+from barhana.rules import demorgan, ideriv, let_not, unconj, vacuous
 
 p,q = symbols('p q')
 pr = [
@@ -11,9 +11,10 @@ pr = [
 conc = p & ~q
 
 cl1 = unconj(conc)[0]
-if1 = let_not(cl1)
-st1 = vacuous(if1, q)
-pf1 = ideriv(st1, pr[0])
+
+if if1 := let_not(cl1):
+    st1 = vacuous(if1, q)
+    pf1 = ideriv(st1, pr[0])
 
 cl2 = unconj(conc)[1]
 if2 = let_not(cl2)
