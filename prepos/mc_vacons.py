a77db5691a24b3ee329297722d03c5ba1c73ce1b
Mustafa Alelg <me@mstfelg.com>
2023-11-10 19:45:36 -0800

    Move material conditional to prepositions

--- prepos/mc_vacons.py
+++ prepos/mc_vacons.py
@@ -1,4 +1,7 @@
-# desc: Law of implication
+'''
+desc: Vacuous consequent implication
+Assume P. Therefore Q implies P.
+'''
 
 from sympy import symbols
 
@@ -6,11 +9,11 @@ from barhana.rules import claim_cons, dderiv, let_ante, theorem
 
 p,q = symbols('p q')
 pr = [
-    q
+    p
 ]
-cl0 = p >> q
+cl0 = q >> p
 thm = theorem(pr, cl0)
 
-if if1 := let_ante(cl0):
-    cl1 = claim_cons(cl0)
+if if1 := let_ante(cl0): # q
+    cl1 = claim_cons(cl0) # p
 pf1 = dderiv(cl1, pr[0])
---
+++ prepos/mc_vacuous.py
@@ -0,0 +1,19 @@
+'''
+desc: Vacuous implication
+Assume P. Therefore, not P implies anything.
+'''
+
+from sympy import symbols
+
+from barhana.rules import claim_cons, ideriv, let_ante, theorem
+
+p,q = symbols('p q')
+pr = [
+    p
+]
+cl0 = ~p >> q
+thm = theorem(pr, cl0)
+
+if if1 := let_ante(cl0):
+    cl1 = claim_cons(cl0)
+pf1 = ideriv(if1, pr[0])
