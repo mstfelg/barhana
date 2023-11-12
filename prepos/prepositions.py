0d0f57035af4b5bd1fb6065c3c08ea99f6732db7
Mustafa Alelg <dev@mstfelg.com>
2023-11-11 23:51:15 -0800

    Reformat material conditional properties

--- prepos/prepositions.py
+++ prepos/prepositions.py
@@ -1,3 +1,3 @@
-from p0_identity import thm as rep
-from p1_implication import thm as vac
-from p2_modt import thm as modt
+from mc_identity import thm as rep
+from mc_vacons import thm as modt
+from mc_vacuous import thm as vac
