6a9c69afa5fe5780e921f79257bc93d62d7bc216
Mustafa Alelg <dev@mstfelg.com>
2023-11-13 17:14:56 -0800

    Add axiom graphs

---
+++ prepos/mc_peirce.py
@@ -0,0 +1,14 @@
+'''
+desc: Peirce's law
+'''
+
+from sympy import symbols
+
+from barhana.rules import claim_cons, ideriv, let_ante, theorem
+
+p,q = symbols('p q')
+pr = [
+    (p >> q) >> p
+]
+cl0 = p
+thm = theorem(pr, cl0)
