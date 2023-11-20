4187d43f3888f8f958d018bd4415d1127407ffc2
Mustafa Alelg <me@mstfelg.com>
2023-11-19 23:33:18 -0800

    Add Peirce theorem proof

--- prepos/mc_peirce.py
+++ prepos/mc_peirce.py
@@ -4,7 +4,8 @@ title: Peirce's law
 
 from sympy import symbols
 
-from barhana.rules import claim_cons, ideriv, let_ante, theorem
+from barhana.rules import (claim_cons, ideriv, let_ante, let_not, modt,
+                           theorem, vacuous)
 
 p,q = symbols('p q')
 pr = [
@@ -12,3 +13,9 @@ pr = [
 ]
 conc = p
 peirce = theorem(pr, conc)
+
+if cl0 := conc: # p
+    if1 = let_not(cl0) # ~p
+    st1 = modt(pr[0], if1) # ~(p >> q)
+    st2 = vacuous(if1, q) # p >> q
+    pf0 = ideriv(if1, st1, st2) # p
